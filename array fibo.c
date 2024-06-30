01481baddd1b1ffe2aa34be94096a88cc5e69c3b
Mohammad Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2024-07-01 01:14:45 +0600

    array fibonacci

---
+++ array fibo.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+int a[30],n,i;
+printf("enter the number of the terms n = ");
+scanf("%d",&n);
+
+
+
+
+
+a[0]=0;
+a[1]=1;
+for(i=2;i<n;i++){
+    a[i]=a[i-1]+a[i-2];
+}
+
+
+for(i=0;i<n;i++){
+
+
+        printf("%d",a[i]);
+
+      if(i<n-1){
+printf(",");
+        }
+
+}
+printf("\n");
+
+
+
+
+   getch();
+}
+
