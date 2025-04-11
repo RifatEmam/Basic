e7e0071ef93f350e148f4f7e43e6d331eba97987
Mohammad Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-04-12 01:38:38 +0600

    squared qubic

---
+++ beecrowed logical sequence.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n,i,j,k;
+    cin>>n;
+
+    int a=1;
+    if(n>1&&n<1000)
+    {
+        for(i=1; i<=n; i++)
+        {
+
+            cout<<a<<" "<<a*a<<" "<<a*a*a<<endl;
+            a++;
+
+
+
+
+        }
+
+    }
+
+
+
+
+    return 0;
+
+
+
+
+
+}
